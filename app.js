20daf7cdd81eea4c7b845e4495f751e0185e3a7c
huhq <fox2081@foxmail.com>
2018-10-31 17:30:03 +0800

    [Mod] remove log system

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const createError = require('http-errors');
-const log4js = require('log4js');
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -12,11 +11,17 @@ app.use(bodyParser.json({limit: '50mb'}));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
+    console.log('---catch 404: ', req, res)
     next(createError(404));
+});
+
+app.all('*', function (req, res, next) {
+    next();
 });
 
 // error handler
 app.use((err, req, res, next) => {
+    console.log('---error handler: ', err, req, res)
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -29,24 +34,28 @@ app.use((err, req, res, next) => {
 app.listen(2018);
 
 app.post('/snap', (req, res) => {
+    console.log('---post snap: ', req, res)
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-    let logger = log4js.getLogger('webrtc');
     let params = req.params;
 
     try {
-        logger.debug(req.rawBody);
+        console.log(req.rawBody);
         let base64Data = req.rawBody.replace(/^data:image\/png;base64,/, '');
-        logger.debug(base64Data);
+        console.log(base64Data);
 
         let result = 'ok';
         fs.writeFile(`./dist/cap-${params.id || 'noId'}.png`, base64Data, 'base64', (err) => {
-            logger.error(err);
+            console.log(err);
             result = err;
         });
 
         res.send(result);
     } catch (ex) {
-        logger.error(ex);
+        console.log(ex);
         res.send(ex);
     }
 });
